91652e58ac4bcb92ddf05e629c1166a801696fff
ian <brokenmetal@hotmail.com>
2019-01-12 13:36:39 -0600

    Lost my unit test on the first commit. This should fix it.

---
+++ PillarKata/UnitTests/ShoppingCartTest.cs
@@ -0,0 +1,21 @@
+﻿using Business;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class ShoppingCartTest
+    {
+        [Test]
+        public void AddItem_AddingSoupWithNoSales_NormalPrice()
+        {
+            //Arrange
+            var cart = new ShoppingCart();
+
+            //Act
+            var total = cart.Add("Soup");
+
+            //Assert
+            Assert.AreEqual(total, 1.89);
+        }
+    }
+}
