993668ad8853d7245f862e0c1d21bfdbb0497d9e
ian <brokenmetal@hotmail.com>
2019-01-13 12:24:33 -0600

    Adding unit tests to makefile.bat

--- BuildAndRun/makefile.bat
+++ BuildAndRun/makefile.bat
@@ -1,2 +1,5 @@
 .\csc /t:library /out:Business.dll /r:System.Core.dll,Microsoft.CodeAnalysis.dll,Microsoft.CodeAnalysis.CSharp.dll,System.Collections.Immutable.dll,System.Text.Encoding.CodePages.dll,System.Reflection.Metadata.dll,System.Runtime.dll /recurse:..\PillarKata\Business\*.cs
 .\csc /out:PillarKata.exe /r:Business.dll,Microsoft.CodeAnalysis.dll,Microsoft.CodeAnalysis.CSharp.dll,System.Collections.Immutable.dll,System.Text.Encoding.CodePages.dll,System.Reflection.Metadata.dll ..\PillarKata\PillarKata\Program.cs
+cd ..\PillarKata\UnitTests\
+..\..\BuildAndRun\dotnet.exe test
+cd ..\..\BuildAndRun\
